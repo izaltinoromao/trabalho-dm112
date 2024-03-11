6879bc5446cf39d71a69b05816941aab7b1315dd
Izaltino <izaltinoromaoneto@hotmail.com>
2024-03-11 19:25:09 -0300

    ✨ Creating EntregaDM112

---
+++ EntregaDM112/src/main/java/br/inatel/dm112/EntregaDM112/EntregaDm112Application.java
@@ -0,0 +1,13 @@
+package br.inatel.dm112.EntregaDM112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaDm112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaDm112Application.class, args);
+	}
+
+}
---
+++ EntregaDM112/src/test/java/br/inatel/dm112/EntregaDM112/EntregaDm112ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.dm112.EntregaDM112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaDm112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
